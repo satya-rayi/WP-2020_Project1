f8fd5033e02d95cd355f71fd70c7700d04fe019e
satyawin007 <satya.rayi@msitprogram.net>
2020-03-26 18:01:28 +0530

    changes made for encrypting password

--- application.py
+++ application.py
@@ -1,10 +1,13 @@
 import os
+import time  
+import hashlib 
 
 from flask import Flask, session, redirect, url_for, request, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from database_setup import User
+
 
 app = Flask(__name__)
 
@@ -30,18 +33,23 @@ def index():
 
 @app.route("/admin")
 def admin():
-    users = db.query(User).all()
+    users = db.query(User).order_by(User.created_at.desc()).all()
     return render_template('users.html',users=users)  
 
 @app.route('/register', methods=['GET', 'POST']) 
 def register():
-    if request.method == 'POST':
-        import time   
+    if request.method == 'POST':        
         username = request.form['username']
         password = request.form["password"]
-        user = User(username=username, password=password, created_at=time.strftime('%Y-%m-%d %H:%M:%S'))
-        db.add(user)
-        db.commit()
+        password = hashlib.md5(password.encode()).hexdigest()
+        
+        exists = db.query(User.id).filter_by(username=username).scalar()
+        if (exists is None) :
+            user = User(username=username, password=password, created_at=time.strftime('%Y-%m-%d %H:%M:%S'))
+            db.add(user)
+            db.commit()
+            return render_template('welcome.html',username=username)        
+        username = ""
         return render_template('welcome.html',username=username)
     else:
         return render_template('register.html')
